6ecd1986eb3b89e37fa280ce57018a8a1bf80ceb
goosling <xiaoliu920@163.com>
2016-07-29 22:54:12 +0800

    transition

---
+++ app/src/main/java/joe/com/musicplayer/view/TransitionAdapter.java
@@ -0,0 +1,34 @@
+package joe.com.musicplayer.view;
+
+import android.transition.Transition;
+
+/**
+ * Created by JOE on 2016/7/29.
+ */
+public class TransitionAdapter implements Transition.TransitionListener {
+
+    @Override
+    public void onTransitionStart(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionEnd(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionCancel(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionPause(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionResume(Transition transition) {
+
+    }
+}

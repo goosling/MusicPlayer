4ed585df55b93533d94693457f1c6049b087e4d6
goosling <xiaoliu920@163.com>
2016-07-30 23:49:02 +0800

    transition

---
+++ app/src/main/java/joe/com/musicplayer/transition/AnimatedVectorDrawableCompat.java
@@ -0,0 +1,11 @@
+package joe.com.musicplayer.transition;
+
+import android.os.Handler;
+
+/**
+ * Created by JOE on 2016/7/30.
+ */
+public class AnimatedVectorDrawableCompat {
+
+    private static final Handler sHandler = new AnimatedVectorDrawableCompat();
+}
